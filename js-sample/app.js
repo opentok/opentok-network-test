953922d1251291ed67c0aef90ba61666d9671a14
James Zhao <james@tokbox.com>
2015-07-15 12:35:12 -0700

    add fix for forEach function below IE10

--- js-sample/app.js
+++ js-sample/app.js
@@ -237,6 +237,14 @@ function compositeOfCallbacks(obj, fns, callback) {
     }
   };
 
+  if ( !Array.prototype.forEach ) {
+      Array.prototype.forEach = function(fn, scope) {
+          for(var i = 0, len = this.length; i < len; ++i) {
+              fn.call(scope, this[i], i, this);
+          }
+      }
+  }
+  
   fns.forEach(function(key) {
     var originalCallback = obj[key];
 
